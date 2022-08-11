b38674a631224ce23210f08bcc2836a13dfc7120
mateus-mos <mateus.silvaoliveira13@outlook.com>
2022-08-11 11:09:21 -0300

    [fix] URDF working

--- taylor_robot_description/launch/taylor.launch.py
+++ taylor_robot_description/launch/taylor.launch.py
@@ -22,6 +22,7 @@ def generate_launch_description():
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
