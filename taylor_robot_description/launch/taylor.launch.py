37543085d42e5da5efe62a8ba829beefa3aa32b1
mateus-mos <mateus.silvaoliveira13@outlook.com>
2022-08-16 11:51:54 -0300

    [change] Trying to fix some bugs

--- taylor_robot_description/launch/taylor.launch.py
+++ taylor_robot_description/launch/taylor.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('taylor_robot_description'))
-    xacro_file = os.path.join(pkg_path, 'urdf', 'taylor.robot.xacro')
+    xacro_file = os.path.join(pkg_path, 'urdf', 'taylor.robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
