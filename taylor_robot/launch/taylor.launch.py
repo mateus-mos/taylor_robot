26f55e5e78ac993b5751e18ddc25495956da65a9
gustavosanches01 <gustavo.sanches7@hotmail.com>
2022-10-17 13:48:16 -0300

    get real taylor robot working

--- taylor_robot/launch/taylor.launch.py
+++ taylor_robot/launch/taylor.launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 import xacro
@@ -12,13 +12,13 @@ def generate_launch_description():
 
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
-
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
     
     
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('taylor_robot'))
     xacro_file = os.path.join(pkg_path, 'urdf', 'taylor.robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
@@ -29,13 +29,14 @@ def generate_launch_description():
         parameters=[params]
     )
 
-
     # Launch!
     return LaunchDescription([
+        
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
+
         DeclareLaunchArgument(
             'use_ros2_control',
             default_value='true',
