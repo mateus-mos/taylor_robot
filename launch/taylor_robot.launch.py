7536a4ba20dca3f64f4d7613b93952bc351fbf0f
mateus-mos <mateus.silvaoliveira13@outlook.com>
2022-11-04 14:25:02 -0300

    [change] Check if the robot_description is set

--- launch/taylor_robot.launch.py
+++ launch/taylor_robot.launch.py
@@ -1,4 +1,7 @@
 import os
+import subprocess
+import time
+import rclpy
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -15,76 +18,83 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    # Check if the robot_description is up 
+    output = subprocess.check_output(["ros2", "topic", "list"])
+    substring = "robot_description"
 
-    package_name='taylor_robot' #<--- CHANGE ME
+    if output.find("robot_description") == -1:
+        rclpy.Node.get_logger().info("Waiting robot_description...")
+        time.sleep(3)
 
-    rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','taylor.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
-    )
+    #print(process.communicate())
 
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+    #package_name='taylor_robot' 
+    #rsp = IncludeLaunchDescription(
+    #            PythonLaunchDescriptionSource([os.path.join(
+    #                get_package_share_directory(package_name),'launch','taylor.launch.py'
+    #            )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+    #)
 
-    controller_manager = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
-                    controller_params_file]
-    )
+    #robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    #controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["diff_cont"],
-    )
+    #controller_manager = Node(
+    #    package="controller_manager",
+    #    executable="ros2_control_node",
+    #    parameters=[{'robot_description': robot_description},
+    #                controller_params_file]
+    #)
 
-    delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[diff_drive_spawner],
-        )
-    )
+    #delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["joint_broad"],
-    )
+    #diff_drive_spawner = Node(
+    #    package="controller_manager",
+    #    executable="spawner.py",
+    #    arguments=["diff_cont"],
+    #)
 
-    delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[joint_broad_spawner],
-        )
-    )
+    #delayed_diff_drive_spawner = RegisterEventHandler(
+    #    event_handler=OnProcessStart(
+    #        target_action=controller_manager,
+    #        on_start=[diff_drive_spawner],
+    #    )
+    #)
 
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+    #joint_broad_spawner = Node(
+    #    package="controller_manager",
+    #    executable="spawner.py",
+    #    arguments=["joint_broad"],
+    #)
+
+    #delayed_joint_broad_spawner = RegisterEventHandler(
+    #    event_handler=OnProcessStart(
+    #        target_action=controller_manager,
+    #        on_start=[joint_broad_spawner],
+    #    )
+    #)
+
+    ## Code for delaying a node (I haven't tested how effective it is)
+    ## 
+    ## First add the below lines to imports
+    ## from launch.actions import RegisterEventHandler
+    ## from launch.event_handlers import OnProcessExit
+    ##
+    ## Then add the following below the current diff_drive_spawner
+    ## delayed_diff_drive_spawner = RegisterEventHandler(
+    ##     event_handler=OnProcessExit(
+    ##         target_action=spawn_entity,
+    ##         on_exit=[diff_drive_spawner],
+    ##     )
+    ## )
+    ##
+    ## Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
 
     # Launch them all!
     return LaunchDescription([
-        rsp,
-        delayed_controller_manager,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
+        #rsp,
+        #delayed_controller_manager,
+        #delayed_diff_drive_spawner,
+        #delayed_joint_broad_spawner
     ])
