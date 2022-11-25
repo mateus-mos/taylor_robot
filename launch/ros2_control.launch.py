f9fd30edfcb27d2f10c46ec228df9fe85a652528
mateus-mos <mateus.silvaoliveira13@outlook.com>
2022-11-25 17:23:52 -0300

    [add] ros2_control launch file

---
+++ launch/ros2_control.launch.py
@@ -0,0 +1,82 @@
+import os
+import subprocess
+import time
+import rclpy
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+
+from launch_ros.actions import Node
+
+def check_robot_description():
+    rclpy.init()
+    # Check if the robot_description is up 
+
+    # Decode because the subproces.check_output return a bytes object
+    output = subprocess.check_output(["ros2", "topic", "list"]).decode("utf-8") 
+
+    while output.find("robot_description") == -1:
+        rclpy.logging.get_logger('Launch File').warn('Waiting for the robot_description node...')
+        output = subprocess.check_output(["ros2", "topic", "list"]).decode("utf-8") 
+        time.sleep(1.0)
+    
+    rclpy.logging.get_logger('Launch File').info('Robot_description node is up!')
+
+def generate_launch_description():
+
+    check_robot_description()
+
+    package_name='taylor_robot' 
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
+    ])
