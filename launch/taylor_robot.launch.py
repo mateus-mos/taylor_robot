d89e406e7fd708a4c6690830aa9549de41231386
mateus-mos <mateus.silvaoliveira13@outlook.com>
2022-11-25 17:23:34 -0300

    [change] Wait_until_up function created. ros2_control launch removed

--- launch/taylor_robot.launch.py
+++ launch/taylor_robot.launch.py
@@ -15,84 +15,36 @@ from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
 
-def check_robot_description():
+def wait_until_up(necessary_nodes):
     rclpy.init()
-    # Check if the robot_description is up 
-
+    
     # Decode because the subproces.check_output return a bytes object
     output = subprocess.check_output(["ros2", "topic", "list"]).decode("utf-8") 
 
-    while output.find("robot_description") == -1:
-        rclpy.logging.get_logger('Launch File').warn('Waiting for the robot_description node...')
-        output = subprocess.check_output(["ros2", "topic", "list"]).decode("utf-8") 
-        time.sleep(1.0)
+    i = 0;
+    dont_find_a_node = False
+
+    while i < len(necessary_nodes):
+        while output.find(necessary_nodes[i]) == -1:
+            rclpy.logging.get_logger('Taylor Robot Launch').warn('Waiting for the ' + necessary_nodes[i] + ' node! ')
+            output = subprocess.check_output(["ros2", "topic", "list"]).decode("utf-8") 
+            dont_find_a_node = True
+            time.sleep(1.0)
+        
+        # If a node was not up, so check again all the nodes
+        if dont_find_a_node == True:
+            i = 0
+        else:
+            i += 1
     
-    rclpy.logging.get_logger('Launch File').info('Robot_description node is up!')
+    rclpy.logging.get_logger('Taylor Robot Launch').info('All necessary nodes are up!')
 
 def generate_launch_description():
+    
+    necessary_nodes = ['robot_description']
 
-    check_robot_description()
-
-    package_name='taylor_robot' 
-
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
-
-    controller_manager = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
-                    controller_params_file]
-    )
-
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
-
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["diff_cont"],
-    )
-
-    delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[diff_drive_spawner],
-        )
-    )
-
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["joint_broad"],
-    )
-
-    delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[joint_broad_spawner],
-        )
-    )
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+    wait_until_up(necessary_nodes)
 
     # Launch them all!
     return LaunchDescription([
-        delayed_controller_manager,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
     ])
