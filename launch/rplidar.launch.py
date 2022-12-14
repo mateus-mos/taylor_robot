4647eb874017c6267279831e13d47d2011a88423
mateus-mos <mateus.silvaoliveira13@outlook.com>
2022-12-14 16:45:42 +0000

    [set] lidar serial port is set again

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1.2:1.0-port0',
+                'serial_port': '/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1.3:1.0-port0',
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
